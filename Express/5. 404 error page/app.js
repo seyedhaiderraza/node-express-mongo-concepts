d9ca70188fdab05afa41c586ba619f6d1351edbe
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-04-02 11:49:50 +0530

    404 error page and footer height adjust with body html container height

---
+++ Express/5. 404 error page/app.js
@@ -0,0 +1,50 @@
+//partials is a file with code to be included in  every page to avoid repeating code
+
+/*
+for using partials:
+1. create template engine const -> const hbs = require('hbs')
+2. create partialsPath
+3. do registerPartials(partialsPath)
+*/
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+const port = 3000
+
+const app = new express()
+
+const partialsPath = path.join(__dirname, "./partials")
+hbs.registerPartials(partialsPath)
+const templatePath = path.join(__dirname, './templatepath')
+const staticFilesPath = path.join(__dirname, "./public")
+
+app.set('view engine', 'hbs')
+app.set('views', templatePath)
+app.use(express.static(staticFilesPath))
+
+
+app.get("/", (req, res) => {
+    res.render('home', {
+        Subscribe: 'HaiderR'
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render('about', {
+
+    })
+})
+app.get('/about/*', (req, res) => {
+        res.render('error', {
+            errormsg: 'Something went wrong while going to about page further!'
+        })
+    })
+    //this creates an error page render route for all routes except defined aboveit
+app.get('*', (req, res) => {
+    res.render('error', {
+        errormsg: 'Something went wrong!'
+    })
+})
+app.listen(port, () => {
+    console.log('server started at', port);
+})
